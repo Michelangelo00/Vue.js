8c0a9f0e2ea22f26c5841e9fb2605a0c00571cc3
michelangelo.cell <michelangelo.cell@NB-MCELLULOSI>
2023-08-17 10:59:42 +0200

    validate forms

--- vue-start-spa/src/components/CreatePage.vue
+++ vue-start-spa/src/components/CreatePage.vue
@@ -1,49 +1,102 @@
 <template>
-    <div class="container mb-3">
-        <form action="">
-            <div class="mb-3">
-                <label for="" class="form-label">
-                    Page Title
-                </label>
-                <!--:value="pageTitle"
-                    @input="(e) => pageTitle = e.target.value"-->
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    v-model="pageTitle"
-                />
+    <form action="" class="container mb-3">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        pageTitle
+                    </label>
+                    <input 
+                        type="text"
+                        class="form-control"
+                        v-model="pageTitle"
+                    >
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Content
+                    </label>
+                    <textarea
+                        type="text"
+                        class="form-control"
+                        rows="5"
+                        v-model="content" 
+                    ></textarea>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="" class="form-label">
-                    Content
-                </label>
-                <textarea 
-                    type="text"
-                    class="form-control" 
-                    rows="5"
-                    v-model="content"
-                ></textarea>
-            </div> 
-            <div class="mb-3">
-                <button 
-                    class="btn btn-primary"
-                    @click.prevent="pageCreated({pageTitle, content})"
-                >
-                    Create Page
-                </button>
+            <div class="col">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Link Text
+                    </label>
+                    <input 
+                        type="text"
+                        class="form-control"
+                        v-model="linkText"
+                    >
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Link URL
+                    </label>
+                    <input 
+                        type="text"
+                        class="form-control"
+                        v-model="linkUrl"
+                    >
+                </div>
+                <div class="row mb-3">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input">
+                        <label for="gridCheck1" class="form-check-label">
+                            Published
+                        </label>
+                    </div>
+                </div>
             </div>
-        </form>
-    </div>
+        </div>
+        <div class="mb-3">
+            <button
+                class="btn btn-primary"
+                @click.prevent="submitForm"
+                :disabled="isFormInvalid"
+                >Create Page</button>
+        </div>
+    </form>
 </template>
 
 <script>
 
 export default {
     props: ['pageCreated'],
+    computed: {
+        isFormInvalid() {
+            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+        }
+    },
     data(){
         return{
             pageTitle: '',
-            content: ''
+            content: '',
+            linkText: '',
+            linkUrl: ''
+        }
+    },
+    methods: {
+        submitForm() {
+            if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                alert("Completa il form!");
+                return;
+            }
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+                link: {
+                    text: this.linkText,
+                    url: this.linkUrl
+                }
+            });
+
         }
     }
 }
