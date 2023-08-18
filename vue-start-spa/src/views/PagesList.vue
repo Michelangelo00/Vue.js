3e2c0d6c1e4ab962995138c1c7007d28c6331a1e
michelangelo.cell <michelangelo.cell@NB-MCELLULOSI>
2023-08-18 10:19:13 +0200

    basic composition API

--- vue-start-spa/src/views/PagesList.vue
+++ vue-start-spa/src/views/PagesList.vue
@@ -1,5 +1,35 @@
 <template>
 
-    <p>This is pages list</p>
+    <p>{{data.counter}}</p>
+    <button @click.prevent="click">Click</button>
 
 </template>
+
+<script setup>
+
+import {ref, reactive} from 'vue';
+
+//const counter = ref(0);
+const data = reactive({counter: 0});
+
+function click () {
+    //counter.value++;
+    data.counter++;
+}
+
+
+/* export default {
+    data() {
+        return {
+            counter: 0
+        };
+    },
+    methods: {
+        click() {
+            this.counter++;
+        }
+    }
+} */
+
+
+</script>
